3563d70f5068510a694449c5239a05e4d79b81fa
leis4ips <leis4ips@mail.ru>
2020-07-01 23:51:20 +0300

    Модификация кода

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -16,11 +16,11 @@ public class GroupCreationTests {
     login("admin", "secret");
   }
 
-  private void login(String admin, String secret) {
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
