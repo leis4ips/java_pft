84430b80aca16b07e7b4c8de213616a6fd271bb9
leis4ips <leis4ips@mail.ru>
2020-07-14 00:05:32 +0300

    Исправляем равенства

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -24,9 +24,9 @@ public class ApplicationManager {
 
   public void init() {
     String browser = BrowserType.FIREFOX;
-    if (browser == BrowserType.FIREFOX) {
+    if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
-    } else if (browser == BrowserType.CHROME) {
+    } else if (browser.equals(BrowserType.CHROME)) {
       wd = new ChromeDriver();
     }
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
