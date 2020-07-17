4d0fd6ad73374aa29ff0f59b7f47ff38f26fb2f8
leis4ips <leis4ips@mail.ru>
2020-07-17 12:45:42 +0300

    Оптимизация заполнения форм (Группы)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -8,7 +8,7 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().createGroup(new GroupData("test1", null, null));
   }
 
 
