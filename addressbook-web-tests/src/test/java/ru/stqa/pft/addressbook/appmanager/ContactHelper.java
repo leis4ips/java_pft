d853f0fd18ad13341c42ea73726eb003038d70ab
leis4ips <leis4ips@mail.ru>
2020-07-21 21:24:17 +0300

    5.5 для Контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -54,16 +53,12 @@ public class ContactHelper extends HelperBase {
     click(By.xpath("//input[@value='Delete']"));
   }
 
-  public void selectContact(int index) {
-    wd.findElements(By.name("selected[]")).get(index).click();
-  }
-
   public void selectContactById(int id) {
     wd.findElement(By.cssSelector("input[value='" + id + "']")).click();
   }
 
-  public void initContactModification(int index) {
-    wd.findElements(By.xpath("//img[@alt='Edit']")).get(index).click();
+  public void initContactModificationById(int id) {
+    wd.findElement(By.cssSelector("a[href='edit.php?id=" + id + "']")).click();
   }
 
   public void submitContactModification() {
@@ -77,17 +72,10 @@ public class ContactHelper extends HelperBase {
     returnToHomePage();
   }
 
-  public void modify(int index, ContactData contact) {
-    initContactModification(index);
+  public void modify(ContactData contact) {
+    initContactModificationById(contact.getId());
     fillContactForm(contact, false);
     submitContactModification();
-    returnToHomePage();
-  }
-
-  public void delete(int index) {
-    selectContact(index);
-    deleteSelectedContacts();
-    confirmSelection();
     returnToHomePage();
   }
 
@@ -104,19 +92,6 @@ public class ContactHelper extends HelperBase {
 
   public int getContactCount() {
    return wd.findElements(By.name("selected[]")).size();
-  }
-
-  public List<ContactData> list() {
-    List<ContactData> contacts = new ArrayList<ContactData>();
-    List<WebElement> elements = wd.findElements(By.name("entry"));
-    for (WebElement element : elements) {
-      List<WebElement> cell = element.findElements(By.tagName("td"));
-        String firstname = cell.get(2).getText();
-        String lastname = cell.get(1).getText();
-        int id = Integer.parseInt(cell.get(0).findElement(By.name("selected[]")).getAttribute("value"));
-        contacts.add(new ContactData().withId(id).withFirstname(firstname).withLastname(lastname));
-      }
-    return contacts;
   }
 
   public Set<ContactData> all() {
