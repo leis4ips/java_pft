946cc62d2fae9b58c8b44e2b3662a2e28fc024f7
leis4ips <leis4ips@mail.ru>
2020-08-05 18:35:55 +0300

    6.10 06:30

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -23,7 +23,6 @@ public class ApplicationManager {
   }
 
   public void init() {
-    String browser = BrowserType.FIREFOX;
     if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
     } else if (browser.equals(BrowserType.CHROME)) {
