f1f4f71d0dc2a5c58931791fe6601f3cca10fd2d
leis4ips <leis4ips@mail.ru>
2020-08-23 13:21:39 +0300

    Задание №20: Реализовать интеграцию тестов с баг-трекером Bugify

---
+++ rest/src/test/java/ru/qa/restbugify/tests/RestTests.java
@@ -0,0 +1,23 @@
+package ru.qa.restbugify.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.qa.restbugify.model.Issue;
+
+import java.io.IOException;
+import java.util.Set;
+
+public class RestTests extends TestBase {
+
+  @Test
+  public void testCreateIssue() throws IOException {
+    skipIfNotFixed(90);
+    Set<Issue> oldIssues = app.rest().getIssuesById(90);
+    Issue newIssue = new Issue().withSubject("First test REST").withDescription("New test issue");
+    int issueId = app.rest().createIssue(newIssue);
+    Set<Issue> newIssues = app.rest().getIssues();
+    oldIssues.add(new Issue().withId(issueId));
+    Assert.assertEquals(newIssues, oldIssues);
+  }
+
+}
---
+++ rest/src/test/java/ru/qa/restbugify/tests/TestBase.java
@@ -0,0 +1,35 @@
+package ru.qa.restbugify.tests;
+
+import org.testng.SkipException;
+import org.testng.annotations.BeforeSuite;
+import ru.qa.restbugify.appmanager.ApplicationManager;
+import ru.qa.restbugify.model.Issue;
+
+import java.io.IOException;
+import java.util.Set;
+
+public class TestBase {
+
+
+  protected static final ApplicationManager app = new ApplicationManager();
+
+  @BeforeSuite
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  public boolean isIssueOpen(int issueId) throws IOException {
+    Set<Issue> issues = app.rest().getIssuesById(issueId);
+    Issue issue = issues.iterator().next();
+    return issue.getState_name().equals("Open")
+            || issue.getState_name().equals("In Progress")
+            || issue.getState_name().equals("Reopened");
+  }
+
+  public void skipIfNotFixed(int issueId) throws IOException {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
+  }
+
+}
